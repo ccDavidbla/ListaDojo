e066b170c6726cf3be35d92dfd1d5b5d7711a5b1
ccDavidbla <davidblanb001@gmail.com>
2024-03-08 15:45:26 -0300

    primer comit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Component from './component';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dojo</h1>
+        <h2>Mi lista de tareas</h2>
+        <Component />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Component() {
+    return (
+        <div>
+            <li>Aprender React</li>
+            <li>Escalar el Monte Everest</li>
+            <li>Correr un maratón</li>
+            <li>Alimentar a los perros</li>
+        </div>
+
+    )
+}
+export default Component;
